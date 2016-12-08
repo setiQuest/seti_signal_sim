74e4466143fc5f508a1f51240a56483ea7b251b4
gadamc <adamcox@us.ibm.com>
2016-12-08 13:46:24 -0800

    updated python code to read data

--- python/read_sim.py
+++ python/read_sim.py
@@ -1,28 +1,31 @@
 import json
 import numpy as np
 
-ff = open('test.data','rb')
+def get_spectrogram(filename, shape=(129,6144)):
+    ff = open(filename,'rb')
+    header = json.loads(ff.readline())
+    raw_data = ff.read()
+    complex_data = np.frombuffer(raw_data, dtype='i1').astype(np.float32).view(np.complex64)
+    complex_data = complex_data.reshape(shape[0],shape[1])
+    cpfft = np.fft.fftshift( np.fft.fft(complex_data), 1)
+    spectrogram = np.abs(cpfft)**2
+    return spectrogram
 
-header = json.loads(ff.readline())
+def scale(spectrogram, max=255.0, min=False):
+  if min:
+    spectrogram = spectrogram - spectrogram.min()
+  spectrogram = max * spectrogram / spectrogram.max()
+  return spectrogram
 
-raw_data = ff.read()
-complex_data = np.frombuffer(raw_data, dtype='i1').astype(np.float32).view(np.complex64)
-
-#reshape to waterfall shape
-complex_data = complex_data.reshape(129,6144)
-
-cpfftd = np.fft.fftshift( np.fft.fft(complext_data), 1)
-spectrogram = np.abs(cpfft)**2
-# isn't this the same and faster?
-# spectrogram = cpfft.real**2 + cpfft.imag**2
+#isn't this faster?
+#spectrogram = cpfft.real**2 + cpfft.imag**2
 
 import matplotlib.pyplot as plt
 plt.ion()
-
 fig, ax = plt.subplots()
 
+spectrogram = get_spectrogram('test.data')
 ax.imshow(spectrogram)
-
 ax.imshow(spectrogram, 
   aspect = 0.5*float(spectrogram.shape[1]) / spectrogram.shape[0])
 
