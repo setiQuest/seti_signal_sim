a2624784619edcc2ce85e9f434851be24884efde
gadamc <adamcox@us.ibm.com>
2016-12-15 11:14:37 -0800

    improved python readout example code

--- python/read_sim.py
+++ python/read_sim.py
@@ -1,12 +1,17 @@
 import json
 import numpy as np
+import matplotlib.pyplot as plt
+plt.ion()
+
+fig, ax = plt.subplots()
 
 def get_spectrogram(filename, shape=(129,6144)):
     ff = open(filename,'rb')
     header = json.loads(ff.readline())
-    raw_data = ff.read()
-    complex_data = np.frombuffer(raw_data, dtype='i1').astype(np.float32).view(np.complex64)
-    complex_data = complex_data.reshape(shape[0],shape[1])
+    #raw_data = ff.read()  
+    #complex_data = np.frombuffer(raw_data, dtype='i1').astype(np.float32).view(np.complex64)
+    complex_data = np.fromfile(ff, dtype='i1').astype(np.float32).view(np.complex64)
+    complex_data = complex_data.reshape(*shape)
     cpfft = np.fft.fftshift( np.fft.fft(complex_data), 1)
     spectrogram = np.abs(cpfft)**2
     return spectrogram
@@ -17,16 +22,26 @@ def scale(spectrogram, max=255.0, min=False):
   spectrogram = max * spectrogram / spectrogram.max()
   return spectrogram
 
+def to_json(fin,fout):
+  ff = open(fin,'rb')
+  header = json.loads(ff.readline())
+  raw_data = ff.read()
+  header['raw_data'] = raw_data
+  json.dump()
 #isn't this faster?
 #spectrogram = cpfft.real**2 + cpfft.imag**2
 
-import matplotlib.pyplot as plt
-plt.ion()
-fig, ax = plt.subplots()
+def read_and_show(filename='test.data', log=False):
+  
+  
 
-spectrogram = get_spectrogram('test.data')
-ax.imshow(spectrogram)
-ax.imshow(spectrogram, 
-  aspect = 0.5*float(spectrogram.shape[1]) / spectrogram.shape[0])
+  spectrogram = get_spectrogram(filename)
+  if log:
+    spectrogram = np.log(spectrogram)
 
-## the only issue at the moment is the reversed frequency bins compared to the PIL library read of the PPM file generated with SETIKit.
+  ax.imshow(spectrogram)
+  ax.imshow(spectrogram, 
+    aspect = 0.5*float(spectrogram.shape[1]) / spectrogram.shape[0])
+
+  return fig, ax
+
