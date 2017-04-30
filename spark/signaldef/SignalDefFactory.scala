2050ca4fbb44d307b0fb3436d9c56891cada87e7
gadamc <adamcox@us.ibm.com>
2017-04-29 18:07:50 -0700

    remove call to next

--- spark/signaldef/SignalDefFactory.scala
+++ spark/signaldef/SignalDefFactory.scala
@@ -9,47 +9,38 @@ object SignalDefFactory {
   def apply(signalClass: String, rand: Random, dataClass: String) : SignalDef = signalClass.toLowerCase match {
     case "test" => {
       val signalDef = new TestSignal(rand,dataClass)
-      signalDef.next
       return signalDef
     }
     case "narrowband" => {
       val signalDef = new NarrowBand(rand,dataClass)
-      signalDef.next
       return signalDef
     }
     case "squarepulsednarrowband" => {
       val signalDef = new SquarePulsedNarrowBand(rand,dataClass)
-      signalDef.next
       return signalDef
     }
     case "squigglesquarepulsednarrowband" => {
       val signalDef = new SquiggleSquarePulsedNarrowBand(rand,dataClass)
-      signalDef.next
       return signalDef
     }
     case "squiggle" => {
       val signalDef = new Squiggle(rand,dataClass)
-      signalDef.next
       return signalDef
     }
     case "narrowbanddrd" => {
       val signalDef = new NarrowBandDRD(rand,dataClass)
-      signalDef.next
       return signalDef
     }
     case "squigglesquarepulsednarrowbanddrd" => {
       val signalDef = new NarrowBandDRD(rand,dataClass)
-      signalDef.next
       return signalDef
     }
     case "brightpixel" => {
       val signalDef = new BrightPixel(rand,dataClass)
-      signalDef.next
       return signalDef
     }
     case "noise" => {
       val signalDef = new PureNoise(rand,dataClass)
-      signalDef.next
       return signalDef
     }
     
