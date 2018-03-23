57e12aad361f50e636e6741710b71d64ec076b2e
gadamc <adamcox@us.ibm.com>
2018-03-23 10:30:56 -0700

    update script

--- build_sims.sh
+++ build_sims.sh
@@ -8,13 +8,18 @@ echo $JAR
 #clean up local directory just in case. 
 rm *.dat 
 
-for i in 0.008 0.01 0.02 0.04 0.06 0.08 0.1 0.2 0.4
+#for i in 0.008 0.01 0.02 0.04 0.06 0.08 0.1 0.2 0.4
+#for i in 0.05 0.07 0.09 0.12 0.16
+for i in 0.4
+#for i in 0.008 0.01 0.02 0.04 0.06 0.08 0.1 0.2 0.4 0.05 0.07 0.09 0.12 0.16
 do
 
-  for j in 'narrowband' 'narrowbanddrd' 'noise' 'squiggle' 'brightpixel' 'squarepulsednarrowband' 'squigglesquarepulsednarrowband'
+  #for j in 'narrowband' 'narrowbanddrd' 'noise' 'squiggle' 'brightpixel' 'squarepulsednarrowband' 'squigglesquarepulsednarrowband'
+  for j in 'narrowband' #'noise'
+  #for j in 'noise' 
   do  
     echo $i $j $1
-    java -jar $JAR 'training' 'local' $1 $j 'gaussian' $i   
+    java -jar $JAR training local $1 $j gaussian $i   
   done
   
   dest="snr_${i}/"
